87764302ae45c2bc296cbd7f1f38cdd69ee6e2bb
CaioG <116986699+CaioGLima@users.noreply.github.com>
2024-05-05 09:58:22 -0300

    Update MainPagexaml.cs

--- MainPagexaml.cs
+++ MainPagexaml.cs
@@ -1,17 +1,22 @@
 //using App1.Services
-using App1.models;
+using Syncfusion.Pdf;
+using Syncfusion.Pdf.Graphics;
 using System;
+using System.IO;
 using Xamarin.Forms;
+using Xamarin.Forms.PancakeView;
 
 public partial class MainPage : ContentPage
 {
-    readonly EscalaGlasgow escalaGlasgow;
+    readonly EscalaGlasgow escalaGlasgow;]
+    readonly SfPdfDocument document;
 
 
     public MainPage()
     {
         InitializeComponent();
         escalaGlasgow = new EscalaGlasgow();
+        document = new SfPdfDocument();
 
         pickerAberturaOcular.SelectedIndexChanged += PickerAberturaOcular_SelectedIndexChanged;
         pickerRespostaMotora.SelectedIndexChanged += PickerRespostaMotora_SelectedIndexChanged;
@@ -47,12 +52,39 @@ public partial class MainPage : ContentPage
 
         int pontuacaoTotal = escalaGlasgow.CalcularPontuacao(pontuacaoAberturaOcular, pontuacaoRespostaVerbal, pontuacaoRespostaMotora);
         lblPontuacao.Text = $"Pontuação total: {pontuacaoTotal}";
+        
+    // Mostrar o pop-up para gerar o PDF
+        MostrarPopUpGerarPDF();
     }
+    
+private async void GerarPDF_Clicked(object sender, EventArgs e)
+{
+    // Lógica para gerar o PDF com as informações da Escala de Glasgow
+    string nomeArquivo = "Exame_Escala_Glasgow.pdf";
+    string caminhoArquivo = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), nomeArquivo);
 
-
-    private void GerarPDF_Clicked(object sender, EventArgs e)
+    using (MemoryStream stream = new MemoryStream())
     {
-        // Lógica para gerar o PDF com as informações da Escala de Glasgow
-    }
-}
+        // Cria o documento PDF
+        using (PdfDocument pdfDocument = new PdfDocument())
+        {
+            // Adiciona uma página ao documento
+            PdfPage page = pdfDocument.Pages.Add();
 
+            // Adiciona o conteúdo ao PDF
+            PdfGraphics graphics = page.Graphics;
+            PdfFont font = new PdfStandardFont(PdfFontFamily.Helvetica, 12);
+            graphics.DrawString(lblPontuacao.Text, font, PdfBrushes.Black, new PointF(10, 10));
+
+            // Salva o PDF no caminho especificado
+            pdfDocument.Save(stream);
+        }
+
+        // Escreve o arquivo PDF no dispositivo
+        File.WriteAllBytes(caminhoArquivo, stream.ToArray());
+    }
+
+    // Mostra uma mensagem ao usuário informando que o PDF foi gerado com sucesso
+    await DisplayAlert("PDF Gerado", "O PDF foi gerado com sucesso!", "OK");
+}
+}
