575b0dea107aec376534653db159e62bcba307d8
CaioG <116986699+CaioGLima@users.noreply.github.com>
2024-06-03 22:03:27 -0300

    Update MainPagexaml.cs

--- MainPagexaml.cs
+++ MainPagexaml.cs
@@ -1,57 +1,27 @@
-//using App1.Services
-using Syncfusion.Pdf;
-using Syncfusion.Pdf.Graphics;
 using System;
-using System.IO;
 using Xamarin.Forms;
-using Xamarin.Forms.PancakeView;
 
-public partial class MainPage : ContentPage
+namespace GlasGow
 {
-    readonly EscalaGlasgow escalaGlasgow;
-    readonly SfPdfDocument document;
-    private string nomeEnfermeiro;
-    private string dataExame;
-    private string nomePaciente;
-    private string numProntuario;
-
-
-    public MainPage()
+    public partial class MainPage : ContentPage
     {
-        InitializeComponent();
-        escalaGlasgow = new EscalaGlasgow();
-        document = new SfPdfDocument();
+        private EscalaGlasgow escalaGlasgow;
+        private string nomeEnfermeiro;
+        private string dataExame;
+        private string nomePaciente;
+        private string numProntuario;
 
-        pickerAberturaOcular.SelectedIndexChanged += PickerAberturaOcular_SelectedIndexChanged;
-        pickerRespostaMotora.SelectedIndexChanged += PickerRespostaMotora_SelectedIndexChanged;
-        pickerRespostaVerbal.SelectedIndexChanged += PickerRespostaVerbal_SelectedIndexChanged;
-    }
-
-    private void BtnInfo_Clicked(object sender, EventArgs e)
-    {
-        // Verifica qual botão foi clicado e atualiza a variável correspondente
-        if(sender is Button clickedButton)
+        public MainPage()
         {
-            if (clickedButton == btnNome)
-            {
-                nomeEnfermeiro = txtNomeEnfermeiro.Text;
-            }
-            else if (clickeButton == btnData)
-            {
-                dataExame = txtDataExame.Text;
-            }
-            else if (clickedButton == btnNomePaciente)
-            {
-                nomePaciente = txtNomePaciente.Text;
-            }
-            else if (clickedButton == btnNumProntuario)
-            {
-                numProntuario = txtNumProntuario.Text;
-            }
-          }  
-    }
+            InitializeComponent();
+            escalaGlasgow = new EscalaGlasgow();
 
-    private void PickerAberturaOcular_SelectedIndexChanged(object sender, EventArgs e)
+            pickerAberturaOcular.SelectedIndexChanged += PickerAberturaOcular_SelectedIndexChanged;
+            pickerRespostaMotora.SelectedIndexChanged += PickerRespostaMotora_SelectedIndexChanged;
+            pickerRespostaVerbal.SelectedIndexChanged += PickerRespostaVerbal_SelectedIndexChanged;
+        }
+
+        private void PickerAberturaOcular_SelectedIndexChanged(object sender, EventArgs e)
         {
             VerificarSelecoes();
         }
@@ -94,6 +64,8 @@ public partial class MainPage : ContentPage
                 int pontuacaoRespostaVerbal = escalaGlasgow.ObterPontuacaoRespostaVerbal(opcaoRespostaVerbal);
                 int pontuacaoRespostaMotora = escalaGlasgow.ObterPontuacaoRespostaMotora(opcaoRespostaMotora);
 
+                Console.WriteLine($"Pontuação Abertura Ocular: {pontuacaoAberturaOcular}, Pontuação Resposta Verbal: {pontuacaoRespostaVerbal}, Pontuação Resposta Motora: {pontuacaoRespostaMotora}");
+
                 int pontuacaoTotal = escalaGlasgow.CalcularPontuacao(pontuacaoAberturaOcular, pontuacaoRespostaVerbal, pontuacaoRespostaMotora);
                 lblPontuacao.Text = $"Pontuação total: {pontuacaoTotal}";
             }
@@ -131,47 +103,49 @@ public partial class MainPage : ContentPage
                 Console.WriteLine(ex);
             }
         }
-    
-private async void GerarPDF_Clicked(object sender, EventArgs e)
-    {
-        // Lógica para gerar o PDF com as informações da Escala de Glasgow
-       string conteudoPDF = $"Nome do Enfermeiro: {nomeEnfermeiro}\n" +
-                             $"Data do Exame: {dataExame}\n" +
-                             $"Nome do Paciente: {nomePaciente}\n" +
-                             $"Número do Prontuário: {numProntuario}\n" +
-                             $"Pontuação Abertura Ocular: {pontuacaoAberturaOcular}\n" +
-                             $"Pontuação Resposta Verbal: {pontuacaoRespostaVerbal}\n" +
-                             $"Pontuação Resposta Motora: {pontuacaoRespostaMotora}\n" +
-                             $"Pontuação Total: {pontuacaoTotal}\n";
 
-        string nomeArquivo = "Exame_Escala_Glasgow.pdf";
-        string caminhoArquivo = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), nomeArquivo);
-
-        using (MemoryStream stream = new MemoryStream())
+        private async void BtnInfo_Clicked(object sender, EventArgs e)
         {
-            // Cria o documento PDF
-            using (PdfDocument pdfDocument = new PdfDocument())
+            if (sender is Button clickedButton)
             {
-                // Adiciona uma página ao documento
-                PdfPage page = pdfDocument.Pages.Add();
+                string inputText = string.Empty;
+                string promptTitle = string.Empty;
 
-                // Adiciona o conteúdo ao PDF
-                PdfGraphics graphics = page.Graphics;
-                PdfFont font = new PdfStandardFont(PdfFontFamily.Helvetica, 12);
-                graphics.DrawString(conteudoPDF, font, PdfBrushes.Black, new PointF(10, 10));
+                switch (clickedButton.Text)
+                {
+                    case "Adicionar Nome do Enfermeiro":
+                        promptTitle = "Nome do Enfermeiro";
+                        inputText = await DisplayPromptAsync(promptTitle, "Digite o nome do enfermeiro:");
+                        nomeEnfermeiro = inputText;
+                        txtNomeEnfermeiro.Text = inputText;
+                        break;
+                    case "Adicionar Data do Exame":
+                        promptTitle = "Data do Exame";
+                        inputText = await DisplayPromptAsync(promptTitle, "Digite a data do exame:");
+                        dataExame = inputText;
+                        txtDataExame.Text = inputText;
+                        break;
+                    case "Adicionar Nome do Paciente":
+                        promptTitle = "Nome do Paciente";
+                        inputText = await DisplayPromptAsync(promptTitle, "Digite o nome do paciente:");
+                        nomePaciente = inputText;
+                        txtNomePaciente.Text = inputText;
+                        break;
+                    case "Adicionar Número do Prontuário":
+                        promptTitle = "Número do Prontuário";
+                        inputText = await DisplayPromptAsync(promptTitle, "Digite o número do prontuário:");
+                        numProntuario = inputText;
+                        txtNumProntuario.Text = inputText;
+                        break;
+                }
 
-                // Salva o PDF no caminho especificado
-                pdfDocument.Save(stream);
+                if (!string.IsNullOrWhiteSpace(inputText))
+                {
+                    DisplayAlert("Informação Adicionada", $"A informação foi adicionada com sucesso: {inputText}", "OK");
+                }
             }
-
-            // Escreve o arquivo PDF no dispositivo
-            File.WriteAllBytes(caminhoArquivo, stream.ToArray());
         }
-
-        // Mostra uma mensagem ao usuário informando que o PDF foi gerado com sucesso
-        await DisplayAlert("PDF Gerado", "O PDF foi gerado com sucesso!", "OK");
     }
-}
 }
 
 
